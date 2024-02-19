6fc2ecfc8f2a28cdfe4e6ee869a4099bb3714d31
Shawn <shawn.chen@geminidata.com>
2024-02-19 14:49:35 +0000

    all

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ df_hourly_m['Date'] = pd.to_datetime(df_hourly_m['Date'])
 # Streamlit interface
 st.title("Gemini Central Console Bot")
 action = st.selectbox("Choose an action:", ["draw_line_chart", "generate_answer"])
-host = "https://af05-34-87-50-186.ngrok-free.app"
+host = "https://bfa6-35-198-243-15.ngrok-free.app"
 
 if action == "draw_line_chart":
     url = host + "/analyse"
